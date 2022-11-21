b4ed8a5c7c86a09628cb3508ecd240a2ecaf62b3
RaThida <100819767+RaThida@users.noreply.github.com>
2022-11-21 17:51:06 +0700

    Admin-partials

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!-- Footer Section Starts -->
+<link rel="stylesheet" href="../css/index.css">
+<div class="footer text-center">
+       <div class="wrapper">
+            <p style="color: rgb(20, 85, 0);">2022 All right reserved, Foot House. Developed By -<a href="">IT TEAM</a></p>
+        </div>
+       </div>
+       <!-- Footer Section Ends -->
+
+    </body>
+</html>
